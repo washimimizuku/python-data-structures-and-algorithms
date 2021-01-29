23f034129931af8a24849e86d00bbf6bb2382cd5
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-01-29 18:59:51 +0100

    Add count_bits to primitive section

---
+++ primitives/count_bits.py
@@ -0,0 +1,11 @@
+def count_bits (x):
+    num_bits = 0
+    
+    while x:
+        num_bits += x & 1
+        x >>= 1
+    
+    return num_bits
+
+assert count_bits(12) == 2
+assert count_bits(123) == 6
