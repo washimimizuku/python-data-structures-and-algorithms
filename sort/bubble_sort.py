4aec26f0d613fdfe4939058de5a5829f5cd69b46
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-01-31 16:42:06 +0000

    Add buble sort script

---
+++ sort/bubble_sort.py
@@ -0,0 +1,12 @@
+def bubble_sort(array):
+    length = len(array)
+    for i in range(length):
+        for j in range(i + 1, length):
+            if array[j] < array[i]:
+                temp = array[j]
+                array[j] = array[i]
+                array[i] = temp
+
+numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]
+bubble_sort(numbers)
+print(numbers)
