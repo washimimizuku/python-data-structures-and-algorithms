56fcb0e4cc1c4727b336003e2ac19434a6dce562
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-01-31 16:07:12 +0000

    Add factorial to recursion

---
+++ recursion/factorial.py
@@ -0,0 +1,13 @@
+def factorial(num):
+    if num == 1:
+        return 1
+    return num * factorial(num-1)
+
+def fact(num):
+    result = 1
+    for i in range(1, num + 1):
+        result = result * i
+    return result
+
+print(factorial(6))
+print(fact(4))
