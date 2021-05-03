ebf33a99715fe9c7a65b4156969c387864459989
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-03 10:51:43 +0200

    Add linked lists

---
+++ linked_lists/linked_list.py
@@ -0,0 +1,79 @@
+class Node():
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList():
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        
+    def append(self, data):
+        new_node = Node(data)
+
+        if self.head == None:
+            self.head = new_node
+            self.tail = self.head
+            self.length = 1
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+            self.length += 1
+    
+    def prepend(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self_head = new_node
+
+        self.length += 1
+
+    def insert(self, index, data):
+        new_node = Node(data)
+        i = 0
+        temp = self.head
+
+        if index >= self.length:
+            self.append(data)
+            return
+
+        if index == 0:
+            self.prepend(data)
+
+        for i in range(self.length):
+            if i == index - 1:
+                temp.next, new_node.next = new_node, temp.next
+                self.length += 1
+                break
+            temp = temp.next
+            i += 1
+
+    def remove(self, index):
+        temp = self.head
+        i = 0
+
+        if index >= self.length:
+            print('Entered wrong index')
+
+        if index == 0:
+            self.head = self.head.next
+            self.length -= 1
+            return
+        
+        for i in range(index):
+            if i == index - 1:
+                temp.next = temp.next.next
+                self.length -= 1
+                break
+            i += 1
+            temp = temp.next
+
+list = LinkedList()
+list.append(10)
+list.append(5)
+list.append(6)
+list.prepend(1)
+list.insert(2,99)
+list.insert(34,23)
+#list.remove(5)
