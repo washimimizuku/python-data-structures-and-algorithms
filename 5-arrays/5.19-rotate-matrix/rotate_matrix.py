febb772f8ec0fca062b157704f40044434cfe09d
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-20 23:12:22 +0200

    Fix rotate matrix

--- 5-arrays/5.19-rotate-matrix/rotate_matrix.py
+++ 5-arrays/5.19-rotate-matrix/rotate_matrix.py
@@ -10,47 +10,61 @@ def rotate_matrix(square_matrix):  # Time: O(n2) | Space: O(1)
     for i in range(len(square_matrix) // 2):
         for j in range(i, matrix_size - i):
             # Perform a 4-way exchange.
-            # Note that A[-i] for i in [0, len(A) - 1]
+            # Note that A[~i] for i in [0, len(A) - 1]
             # is A[-(i + 1)].
             (
                 square_matrix[i][j],
-                square_matrix[-j][i],
-                square_matrix[-i][-j],
-                square_matrix[j][-i]
+                square_matrix[~j][i],
+                square_matrix[~i][~j],
+                square_matrix[j][~i]
             ) = (
-                square_matrix[-j][i],
-                square_matrix[-i][-j],
-                square_matrix[j][-i],
+                square_matrix[~j][i],
+                square_matrix[~i][~j],
+                square_matrix[j][~i],
                 square_matrix[i][j]
             )
 
     return square_matrix
 
 
-matrix = [
+matrix2 = [
+    [1, 2],
+    [3, 4]
+]
+
+matrix4 = [
     [1, 2, 3, 4],
     [5, 6, 7, 8],
     [9, 10, 11, 12],
     [13, 14, 15, 16]
 ]
 
-print(rotate_matrix(matrix))
-# NOT WORKING!!!
-# Should be [[13, 9, 5, 1], [14, 10, 6, 2], [15, 11, 7, 3], [16, 12, 8, 4]]
+assert(rotate_matrix(matrix2) == [[3, 1], [4, 2]])
+assert(rotate_matrix(matrix4) == [[13, 9, 5, 1], [
+       14, 10, 6, 2], [15, 11, 7, 3], [16, 12, 8, 4]])
 
 
 class RotateMatrix:
     def __init__(self, square_matrix):
         self._square_matrix = square_matrix
 
-    def read_emtry(self, i, j):
-        # Note that A[-i] for i in [0, len(A) - 1] is A[-(i + 1)].
-        return self._square_matrix[-j][i]
+    def read_entry(self, i, j):
+        # Note that A[~i] for i in [0, len(A) - 1] is A[-(i + 1)].
+        return self._square_matrix[~j][i]
 
     def write_entry(self, i, j, v):
-        self._square_matrix[-j][i] = v
+        self._square_matrix[~j][i] = v
 
 
-matrix_obj = RotateMatrix(matrix)
-print(matrix_obj.read_emtry(0, 0))
-print(matrix_obj.read_emtry(3, 3))
+matrix2 = [
+    [1, 2],
+    [3, 4]
+]
+
+matrix_obj = RotateMatrix(matrix2)
+result = [
+    [matrix_obj.read_entry(0, 0), matrix_obj.read_entry(0, 1)],
+    [matrix_obj.read_entry(1, 0), matrix_obj.read_entry(1, 1)]
+]
+
+assert(result == [[3, 1], [4, 2]])
