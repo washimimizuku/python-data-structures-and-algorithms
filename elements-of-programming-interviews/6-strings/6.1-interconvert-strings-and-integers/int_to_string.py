6e542fd370b8dd2a01edf2f7b0e57c3b1dd4dd3c
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-07-05 15:15:50 +0200

    Add int to string conversion to strings

---
+++ elements-of-programming-interviews/6-strings/6.1-interconvert-strings-and-integers/int_to_string.py
@@ -0,0 +1,24 @@
+'''
+Implement an integer to string conversion function
+'''
+
+
+def int_to_string(x):  # Time: O(n)
+    is_negative = False
+    if x < 0:
+        x, is_negative = -x, True
+
+    s = []
+    while True:
+        s.append(chr(ord('0') + x % 10))
+        x //= 10
+        if x == 0:
+            break
+
+    # Adds the negative sign back is is_negative
+    return('-' if is_negative else'') + ''.join(reversed(s))
+
+
+assert(int_to_string(-123) == '-123')
+assert(int_to_string(0) == '0')
+assert(int_to_string(123) == '123')
