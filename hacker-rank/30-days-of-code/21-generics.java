df101249e982d001b741c03070de42bed04b899d
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-09-20 13:59:18 +0200

    Add generics to HackerRank

--- hacker-rank/30-days-of-code/21-generics.java
+++ hacker-rank/30-days-of-code/21-generics.java
@@ -13,12 +13,10 @@ import java.util.*;
 class Printer <T> {
 
     /**
-    *    Method Name: printArray
-    *    Print each element of the generic array on a new line. Do not return anything.
-    *    @param A generic array
-    **/
-    
-    // Write your code here
+     *    Method Name: printArray
+     *    Print each element of the generic array on a new line. Do not return anything.
+     *    @param A generic array
+     **/
     public void printArray(T[] arr) {
         for(T each: arr)
             System.out.println(each);
