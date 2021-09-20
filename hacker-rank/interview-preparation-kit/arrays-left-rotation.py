83bf0d00b6811504006b1ed637d61e86afcabbf1
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-09-20 11:25:10 +0200

    Add arrays left rotation to HackerRank

---
+++ hacker-rank/interview-preparation-kit/arrays-left-rotation.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+'''
+https://www.hackerrank.com/challenges/ctci-array-left-rotation
+'''
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def rot_left(a, d):
+    return a[d:] + a[:d]
+
+
+if __name__ == '__main__':
+    result = rot_left([1, 2, 3, 4, 5], 4)
+    assert(result == [5, 1, 2, 3, 4])
