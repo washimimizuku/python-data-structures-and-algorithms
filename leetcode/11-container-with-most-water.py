d1336dc5f174a99e9424d709546a788f26089243
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-09-20 10:12:05 +0200

    Add container with most water to leetcode

---
+++ leetcode/11-container-with-most-water.py
@@ -0,0 +1,69 @@
+'''
+https://leetcode.com/problems/container-with-most-water/
+
+Given n non-negative integers a1, a2, ..., an , where each
+represents a point at coordinate (i, ai). n vertical lines
+are drawn such that the two endpoints of the line i is at
+(i, ai) and (i, 0). Find two lines, which, together with the
+x-axis forms a container, such that the container contains
+the most water.
+
+Notice that you may not slant the container.
+
+Example 1:
+
+8   *_________*____
+7  |*         *   *|
+6  |* *       *   *|
+5  |* *   *   *   *|
+4  |* *   * * *   *|
+3  |* *   * * * * *|
+2  |* * * * * * * *|
+1 *|* * * * * * * *|
+
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
+Explanation: The above vertical lines are represented
+by array [1,8,6,2,5,4,8,3,7]. In this case, the max area
+of water (square section) the container can contain is 49.
+
+Example 2:
+
+Input: height = [1,1]
+Output: 1
+
+Example 3:
+
+Input: height = [4,3,2,1,4]
+Output: 16
+
+Example 4:
+
+Input: height = [1,2,1]
+Output: 2
+'''
+
+from typing import List
+
+
+class Solution:
+    def max_area(self, height: List[int]) -> int:
+        biggest_area = 0
+        begin = 0
+        end = len(height)-1
+
+        while begin < end:
+            if height[begin] > height[end]:
+                area = (end - begin) * height[end]
+            else:
+                area = (end - begin) * height[begin]
+
+            if area > biggest_area:
+                biggest_area = area
+
+            if height[begin] < height[end]:
+                begin += 1
+            else:
+                end -= 1
+
+        return biggest_area
