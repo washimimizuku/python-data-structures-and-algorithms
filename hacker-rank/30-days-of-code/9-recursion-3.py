9e4237289343422bea2826403c36571958550110
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-09-20 12:55:24 +0200

    Add recursion to HackerRank

---
+++ hacker-rank/30-days-of-code/9-recursion-3.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+'''
+https://www.hackerrank.com/challenges/30-recursion
+'''
+
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+
+if __name__ == '__main__':
+    n = int(input())
+
+    result = factorial(n)
+
+    print(str(result) + '\n')
