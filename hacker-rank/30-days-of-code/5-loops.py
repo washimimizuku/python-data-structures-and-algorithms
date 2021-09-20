d6ed685b6fc61086f1c52f33ad82cb56e382e9e4
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-09-20 12:48:13 +0200

    Add loops to HackerRank

---
+++ hacker-rank/30-days-of-code/5-loops.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+
+'''
+https://www.hackerrank.com/challenges/30-loops
+'''
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1, 11):
+        print(n, 'x', i, '=', n * i)
