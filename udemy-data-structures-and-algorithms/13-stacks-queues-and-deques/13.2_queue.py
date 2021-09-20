323b8cf718c393e7d0adb77a9e253e9e656e09b0
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-09-20 18:01:25 +0200

    Add queue implementation

---
+++ udemy-data-structures-and-algorithms/13-stacks-queues-and-deques/13.2_queue.py
@@ -0,0 +1,25 @@
+class Queue:
+
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        self.items.insert(0, item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+
+q = Queue()
+print(q.size())
+
+print(q.is_empty())
+
+q.enqueue(1)
+print(q.dequeue())
