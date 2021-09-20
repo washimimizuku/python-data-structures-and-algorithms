f48292f1da1fd33d1d9b16b0d32a05b231a93768
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-09-20 09:51:20 +0200

    Add two sum to leetcode

---
+++ leetcode/1-two-sum.py
@@ -0,0 +1,27 @@
+'''
+https://leetcode.com/problems/two-sum/
+
+Given an array of integers nums and an integer target, return
+indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution,
+and you may not use the same element twice.
+
+You can return the answer in any order.
+'''
+
+from typing import List
+
+
+def two_sum(nums: List[int], target: int) -> List[int]:
+    past = {}
+    for i in range(0, len(nums)):
+        if nums[i] in past.keys():
+            return[past[nums[i]], i]
+        past[target - nums[i]] = i
+    return []
+
+
+assert(two_sum([2, 7, 11, 15], 9) == [0, 1])
+assert(two_sum([3, 2, 4], 6) == [1, 2])
+assert(two_sum([3, 3], 6) == [0, 1])
