9f3b157d6f06d4e623ecea55bbf6d98d6eeb3b8d
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-09-20 13:47:36 +0200

    Add more exceptions to HackerRank

--- hacker-rank/30-days-of-code/17-more-exceptions.py
+++ hacker-rank/30-days-of-code/17-more-exceptions.py
@@ -1,4 +1,6 @@
 '''
+https://www.hackerrank.com/challenges/30-more-exceptions/
+
 Write a Calculator class with a single method: int power(int,int).
 The power method takes two integers, n and p, as parameters and
 returns the integer result of n^p. If either n or p is negative,
