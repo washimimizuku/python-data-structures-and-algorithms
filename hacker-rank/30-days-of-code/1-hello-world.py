24e4347f2567929abd2b5c196dbc39743d4992ce
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-09-20 11:33:46 +0200

    Add hello world to HackerRank

--- hacker-rank/30-days-of-code/1-hello-world.py
+++ hacker-rank/30-days-of-code/1-hello-world.py
@@ -1,3 +1,7 @@
+'''
+https://www.hackerrank.com/challenges/30-hello-world/
+'''
+
 inputString = input()  # get a line of input from stdin and save it to our variable
 
 # Your first line of output goes here
