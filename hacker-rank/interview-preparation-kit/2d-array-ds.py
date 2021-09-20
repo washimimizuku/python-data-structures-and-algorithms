58f6306a0c30d02ac65759110d28a7e5cc07ee77
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-09-20 11:20:05 +0200

    Add 2D Array - DS to HackerRank

---
+++ hacker-rank/interview-preparation-kit/2d-array-ds.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+'''
+https://www.hackerrank.com/challenges/2d-array
+'''
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def hourglass_sum(arr):
+    total = float('-inf')
+    side = len(arr) - 2
+
+    for i in range(side):
+        for j in range(side):
+            temp_total = 0
+            temp_total += arr[i][j] + arr[i][j+1] + arr[i][j+2]
+            temp_total += arr[i+1][j+1]
+            temp_total += arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
+
+            if total < temp_total:
+                total = temp_total
+
+    return total
+
+
+if __name__ == '__main__':
+    arr = [
+        [1, 1, 1, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [1, 1, 1, 0, 0, 0],
+        [0, 0, 2, 4, 4, 0],
+        [0, 0, 0, 2, 0, 0],
+        [0, 0, 1, 2, 4, 0],
+    ]
+    result = hourglass_sum(arr)
+    assert(result == 19)
