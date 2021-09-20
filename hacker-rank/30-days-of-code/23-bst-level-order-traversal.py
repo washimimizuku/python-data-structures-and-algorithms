3f449bdc1ed4b3f8365f4f9d0f94fea200007983
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-09-20 14:09:56 +0200

    Add bst level order traversal to HackerRank

--- hacker-rank/30-days-of-code/23-bst-level-order-traversal.py
+++ hacker-rank/30-days-of-code/23-bst-level-order-traversal.py
@@ -1,4 +1,6 @@
 '''
+https://www.hackerrank.com/challenges/30-binary-trees
+
 A level-order traversal, also known as a breadth-first search,
 visits each level of a tree's nodes from left to right, top to
 bottom. You are given a pointer, root, pointing to the root of
