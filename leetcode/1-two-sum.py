cdfe9e0100a437537607acc0cdaeb7a3a2f6cb5a
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-09-20 09:52:57 +0200

    Add two sum to leetcode

--- leetcode/1-two-sum.py
+++ leetcode/1-two-sum.py
@@ -13,15 +13,18 @@ You can return the answer in any order.
 from typing import List
 
 
-def two_sum(nums: List[int], target: int) -> List[int]:
-    past = {}
-    for i in range(0, len(nums)):
-        if nums[i] in past.keys():
-            return[past[nums[i]], i]
-        past[target - nums[i]] = i
-    return []
+class Solution:
+    def two_sum(self, nums: List[int], target: int) -> List[int]:
+        past = {}
+        for i in range(0, len(nums)):
+            if nums[i] in past.keys():
+                return[past[nums[i]], i]
+            past[target - nums[i]] = i
+        return []
 
 
-assert(two_sum([2, 7, 11, 15], 9) == [0, 1])
-assert(two_sum([3, 2, 4], 6) == [1, 2])
-assert(two_sum([3, 3], 6) == [0, 1])
+solution = Solution()
+
+assert(solution.two_sum([2, 7, 11, 15], 9) == [0, 1])
+assert(solution.two_sum([3, 2, 4], 6) == [1, 2])
+assert(solution.two_sum([3, 3], 6) == [0, 1])
