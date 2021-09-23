b5706af57814fcaec782352f4af95761c75e9f6f
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-09-23 14:14:51 +0200

    Add binary tree example

--- udemy-data-structures-and-algorithms/16-trees/16.2_binary_tree.py
+++ udemy-data-structures-and-algorithms/16-trees/16.2_binary_tree.py
@@ -43,7 +43,14 @@ class BinaryTree(object):
 
 
 r = BinaryTree('a')
+
 print(r.get_root_val())
 print(r.get_left_child())
 r.insert_left('b')
+print(r.get_left_child())
 print(r.get_left_child().get_root_val())
+r.insert_right('c')
+print(r.get_right_child())
+print(r.get_right_child().get_root_val())
+r.get_right_child().set_root_val('hello')
+print(r.get_right_child().get_root_val())
