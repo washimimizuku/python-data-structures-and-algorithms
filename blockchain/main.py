a0e410b9f8f600ebb662675a81c5bb13f462b8d9
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-11-08 08:47:31 +0100

    Add proof of work

--- blockchain/main.py
+++ blockchain/main.py
@@ -9,6 +9,7 @@ class Block:
         self.data = [] if data is None else data
         self.hash = self.get_hash()
         self.previous_hash = None
+        self.nonce = 0
 
     def get_hash(self):
         new_hash = sha256()
@@ -16,8 +17,18 @@ class Block:
         new_hash.update(str(self.previous_hash).encode('utf-8'))
         new_hash.update(str(self.timestamp).encode('utf-8'))
         new_hash.update(str(self.data).encode('utf-8'))
+        new_hash.update(str(self.nonce).encode('utf'))
 
         return new_hash.hexdigest()
+
+    def mine(self, difficulty):
+        # Basically, it loops until our hash starts with
+        # the string 0...000 with length of <difficulty>.
+        while self.hash[:difficulty] != '0' * difficulty:
+            # We increase our nonce so that we can get a whole different hash.
+            self.nonce += 1
+            # Update our new hash with the new nonce value.
+            self.hash = self.get_hash()
 
 
 class Blockchain:
@@ -25,6 +36,7 @@ class Blockchain:
     def __init__(self):
         # This property will contain all the blocks
         self.chain = [Block(str(int(time())))]
+        self.difficulty = 1
 
     def get_last_block(self):
         return self.chain[len(self.chain) - 1]
@@ -34,6 +46,7 @@ class Blockchain:
         block.previous_hash = self.get_last_block().hash
         # Since now previous_hash has a value, we must reset the block's hash
         block.hash = block.get_hash()
+        block.mine(self.difficulty)
 
         self.chain.append(block)
 
