15ac95b7878d50d0a98f47d12646784a942fae32
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-11-08 08:38:36 +0100

    Add Blockchain class

--- blockchain/main.py
+++ blockchain/main.py
@@ -6,14 +6,33 @@ class Block:
 
     def __init__(self, timestamp=None, data=None):
         self.timestamp = timestamp or time()
-        # Data can contain any information, for example, transactions.
         self.data = [] if data is None else data
+        self.hash = self.get_hash()
+        self.previous_hash = None
 
     def get_hash(self):
-        current_hash = sha256()
+        new_hash = sha256()
 
-        current_hash.update(str(self.previous_hash).encode('utf-8'))
-        current_hash.update(str(self.timestamp).encode('utf-8'))
-        current_hash.update(str(self.data).encode('utf-8'))
+        new_hash.update(str(self.previous_hash).encode('utf-8'))
+        new_hash.update(str(self.timestamp).encode('utf-8'))
+        new_hash.update(str(self.data).encode('utf-8'))
 
-        return current_hash.hexdigest()
+        return new_hash.hexdigest()
+
+
+class Blockchain:
+
+    def __init__(self):
+        # This property will contain all the blocks
+        self.chain = [Block(str(int(time())))]
+
+    def get_last_block(self):
+        return self.chain[len(self.chain) - 1]
+
+    def add_block(self, block):
+        # Since we are adding a new block, previous_hash will be the hash of the old latest block
+        block.previous_hash = self.get_last_block().hash
+        # Since now previous_hash has a value, we must reset the block's hash
+        block.hash = block.get_hash()
+
+        self.chain.append(block)
