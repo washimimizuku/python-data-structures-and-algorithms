d07ae116c80e117f331d990db64f03a5c08b9dff
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-11-08 08:32:36 +0100

    Add get_hash method to Block class

--- blockchain/main.py
+++ blockchain/main.py
@@ -1,3 +1,4 @@
+from hashlib import sha256
 from time import time
 
 
@@ -5,4 +6,14 @@ class Block:
 
     def __init__(self, timestamp=None, data=None):
         self.timestamp = timestamp or time()
+        # Data can contain any information, for example, transactions.
         self.data = [] if data is None else data
+
+    def get_hash(self):
+        current_hash = sha256()
+
+        current_hash.update(str(self.previous_hash).encode('utf-8'))
+        current_hash.update(str(self.timestamp).encode('utf-8'))
+        current_hash.update(str(self.data).encode('utf-8'))
+
+        return current_hash.hexdigest()
